798f03e3c07687f1c26eba3b4df611606386b8d9
Jamie-Cheon <jamiecheon55@gmail.com>
2021-02-28 18:07:06 -0800

    fix bugs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Route } from "react-router-dom";
+import { Route, BrowserRouter } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Detail from "./routes/Detail";
@@ -8,12 +8,12 @@ import "./App.css";
 
 function App(){
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" exact={true} component={About} />
       <Route path="/movie/:id" exact={true} component={Detail} />
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 export default App;
